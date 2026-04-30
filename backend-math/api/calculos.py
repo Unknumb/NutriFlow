6cef7d5a7fb607a39af1dd7614d362ca8bebdde6
Alvaro Uribe <unk@MacBook-Pro-de-Alvaro.local>
2026-04-30 18:26:05 -0400

    feat: implementando cuadrador dinamico de macronutrientes

--- backend-math/api/calculos.py
+++ backend-math/api/calculos.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from schemas.calculos import DatosPaciente, ResultadoTMB
+from schemas.cuadrador import InputCuadrador, OutputCuadrador
 from services.calculadora_tmb import calcular_todas_tmb
+from services.distribucion_service import calcular_distribucion_macros
 
 router = APIRouter()
 
@@ -17,4 +19,18 @@ def calcular_tmb(datos: DatosPaciente):
         porcentaje_grasa=datos.porcentaje_grasa
     )
     
+    return resultado
+
+@router.post("/cuadrador", response_model=OutputCuadrador, summary="Cuadrador Dinámico de Macronutrientes")
+def cuadrar_macros(datos: InputCuadrador):
+    resultado = calcular_distribucion_macros(
+        estrategia_calculo=datos.estrategia_calculo,
+        peso_kg=datos.peso_kg,
+        calorias_totales=datos.calorias_totales,
+        pct_proteina=datos.porcentaje_proteina,
+        pct_grasa=datos.porcentaje_grasa,
+        pct_carbos=datos.porcentaje_carbohidrato,
+        g_kg_proteina=datos.g_kg_proteina,
+        g_kg_grasa=datos.g_kg_grasa
+    )
     return resultado

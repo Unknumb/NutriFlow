f366f653750bc866597657fd6d4a1c5fecb24d6a
Gonzalez230 <benjagonzalez230@gmail.com>
2026-05-07 16:41:48 -0400

    Revert "Feature/docker"

--- backend-core/src/pacientes/pacientes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PacientesController } from './pacientes.controller';
-import { PacientesService } from './pacientes.service';
-
-@Module({
-  controllers: [PacientesController],
-  providers: [PacientesService]
-})
-export class PacientesModule {}
--- backend-core/src/pautas/dto/update-pauta.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePautaDto } from './create-pauta.dto';
-
-export class UpdatePautaDto extends PartialType(CreatePautaDto) {}
